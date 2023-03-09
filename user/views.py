38accb69bb0aebaa9deefd5cc30c0a3a9aae4769
Muratoksuz01 <muratoksuz208@gmail.com>
2023-03-09 15:39:07 +0300

    son değişiklikler bitti

--- user/views.py
+++ user/views.py
@@ -44,6 +44,8 @@ def Register(request):
     return render(request,"user/register.html")
 
 def Login(request):
+    if request.is_authenticated:
+        return redirect("index")
     if request.method=="POST":
         username=request.POST["username"]
         password=request.POST["password"]
@@ -51,72 +53,83 @@ def Login(request):
         if user is not None:
             login(request,user)
             return redirect("index")
-        
-
     return render(request,"user/login.html")
+      
 def Logout(request):
-    logout(request)
-    return redirect("index")
+    if request.is_authenticated:
+        logout(request)
+        return redirect("index")
+    return redirect("login")
 
 def Change_Info(request,pk):
-    user=User.objects.get(id=pk)
-    if request.method=="POST":
-        username=request.POST["username"]
-        email=request.POST["email"]
-        first_name=request.POST["firstname"]
-        last_name=request.POST["lastname"]
-        if user.email!=email:
-            if User.objects.filter(email=email).exists():
-                return render(request,f"user/change.html",{"msg":"kullanıcı mevcut lütfen baska email girniz",
-                "email":email,
-                "first_name":first_name,
-                "last_name":last_name,})
-            user.email=email
-            user.save()
-        if user.username!=username:
-            if User.objects.filter(username=username).exists():
-                return render(request,f"user/change.html",{"msg":"kullanıcı mevcut lütfen baska username girniz",
-                "email":email,
-                "first_name":first_name,
-                "last_name":last_name,})
-            user.username=str(username)
-            user.save()
-        if user.first_name!=first_name:
-            user.first_name=first_name
-            user.save()
-        if user.last_name!=last_name:
-            user.last_name=last_name
-            user.save()                       
-        return redirect("index")
-         
-    return render(request,"user/change.html",{ "email":user.email,
-                    "username":user.username,
-                    "firstname":user.first_name,
-                    "lastname":user.last_name,})
-
+    if request.is_authenticated:
+        user=User.objects.get(id=pk)
+        if request.method=="POST":
+            username=request.POST["username"]
+            email=request.POST["email"]
+            first_name=request.POST["firstname"]
+            last_name=request.POST["lastname"]
+            if user.email!=email:
+                if User.objects.filter(email=email).exists():
+                    return render(request,f"user/change.html",{"msg":"kullanıcı mevcut lütfen baska email girniz",
+                    "email":email,
+                    "first_name":first_name,
+                    "last_name":last_name,})
+                user.email=email
+                user.save()
+            if user.username!=username:
+                if User.objects.filter(username=username).exists():
+                    return render(request,f"user/change.html",{"msg":"kullanıcı mevcut lütfen baska username girniz",
+                    "email":email,
+                    "first_name":first_name,
+                    "last_name":last_name,})
+                user.username=str(username)
+                user.save()
+            if user.first_name!=first_name:
+                user.first_name=first_name
+                user.save()
+            if user.last_name!=last_name:
+                user.last_name=last_name
+                user.save()                       
+            return redirect("index")
+            
+        return render(request,"user/change.html",{ "email":user.email,
+                        "username":user.username,
+                        "firstname":user.first_name,
+                        "lastname":user.last_name,
+                        "disabled1":"disabled"})
+    else:
+        return redirect("login")
 
 def Change_Password(request,pk):# 2. form buraya gelmiyor 
-    user=User.objects.get(id=pk)
-    if request.method=="POST":
-        current_pas=request.POST["current_password"]
-        new_password=request.POST["new_password"]
-        again_password=request.POST["again_password"]
-        if user.check_password(current_pas):
-            if new_password==again_password:
-                user.set_password(new_password)
-                user.save()
-                login(request,user)
-                return redirect("index")
+    if request.is_authenticated:
+        user=User.objects.get(id=pk)
+        if request.method=="POST":
+            current_pas=request.POST["current_password"]
+            new_password=request.POST["new_password"]
+            again_password=request.POST["again_password"]
+            if user.check_password(current_pas):
+                if new_password==again_password:
+                    user.set_password(new_password)
+                    user.save()
+                    login(request,user)
+                    return redirect("index")
+                else:
+                    return render(request,"user/change.html",{"msg":"passwords are not same"})
             else:
-                return render(request,"user/change.html",{"msg":"passwords are not same"})
-        else:
-            return render(request,"user/change.html",{"msg":"old password is not true"})
-    return render(request,"user/change.html")
+                return render(request,"user/change.html",{"msg":"old password is not true"})
+        return render(request,"user/change.html",{"disabled2":"disabled"})
+    else:
+        return redirect("login")
 
 def delete_user(request,pk):
-    user=User.objects.get(id=pk)
-    user.delete()
-    return redirect("index")
+    if request.is_authenticated:
+        user=User.objects.get(id=pk)
+        user.delete()
+        return redirect("index")
+    else:
+        return redirect("login")
+
 
 
    
